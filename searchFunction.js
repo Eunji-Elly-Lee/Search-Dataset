4728811b0ce7306012761ecd218965038cdda8ce
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-05-10 12:54:18 -0600

    Update searchFunction.js

--- searchFunction.js
+++ searchFunction.js
@@ -5,19 +5,19 @@ function SearchCameraId(cameraId) {
 	ToggleClassState("output_field", "hidden", false); 
 	ClearTemplateCollection("cards");
 
-	if(cameraId && cameraId.length > 0) {
+	if (cameraId && cameraId.length > 0) {
 		var searchingCameraId;
 		var matchNumber = 0;
         
-		for(var idx = 0; idx < dataSet.length; idx++) {
+		for (var idx = 0; idx < dataSet.length; idx++) {
 			var obj = dataSet[idx];
 			searchingCameraId = obj.id;
 
-			if(searchingCameraId == cameraId) {
+			if (searchingCameraId == cameraId) {
 				var newCardId = `card-${matchNumber}`;
 				var newCard = GetNewTemplate("card_template", newCardId);
 
-				if(newCard) {
+				if (newCard) {
 					SetTemplateElementValue(newCard, "card_id", (matchNumber + 1));
 					SetTemplateElementValue(newCard, "card_camera_id", obj.id);
 					SetTemplateElementValue(newCard, "card_detail_location", obj.description);
@@ -36,7 +36,7 @@ function SearchCameraId(cameraId) {
 			}
 		}
 
-		if(matchNumber == 0) {
+		if (matchNumber == 0) {
 			SetElementInnerHTML("cards", `No matches found for '${cameraId}'`);
 		}
 	}
@@ -47,20 +47,20 @@ function SearchQuadrantLocation(quadrantLocation) {
 	ToggleClassState("output_field", "hidden", false); 
 	ClearTemplateCollection("cards");
 
-	if(quadrantLocation && quadrantLocation.length > 0) {
+	if (quadrantLocation && quadrantLocation.length > 0) {
 		var searchingQuadrantLocation;
 		var matchNumber = 0;
         quadrantLocation = quadrantLocation.toLowerCase();
 
-		for(var idx = 0; idx < dataSet.length; idx++) {
+		for (var idx = 0; idx < dataSet.length; idx++) {
 			var obj = dataSet[idx];
 			searchingQuadrantLocation = obj.quadrant.toLowerCase();
 
-			if(searchingQuadrantLocation == quadrantLocation) {
+			if (searchingQuadrantLocation == quadrantLocation) {
 				var newCardId = `card-${matchNumber}`;
 				var newCard = GetNewTemplate("card_template", newCardId);
 
-				if(newCard) {
+				if (newCard) {
 					SetTemplateElementValue(newCard, "card_id", (matchNumber + 1));
 					SetTemplateElementValue(newCard, "card_camera_id", obj.id);
 					SetTemplateElementValue(newCard, "card_detail_location", obj.description);
@@ -79,7 +79,7 @@ function SearchQuadrantLocation(quadrantLocation) {
 			}
 		}
 
-		if(matchNumber == 0) {
+		if (matchNumber == 0) {
 			SetElementInnerHTML("cards", `No matches found for '${quadrantLocation}'`);
 		}
 	}
@@ -90,22 +90,22 @@ function SearchDetailLocation(detailLocation) {
 	ToggleClassState("output_field", "hidden", false);
 	ClearTemplateCollection("cards");
 
-	if(detailLocation && detailLocation.length > 0) {
+	if (detailLocation && detailLocation.length > 0) {
 		var searchingDetailLocation; 
 		var matchNumber = 0;
 		detailLocation = detailLocation.toLowerCase();
 
-		for(var idx = 0; idx < dataSet.length; idx++) {
+		for (var idx = 0; idx < dataSet.length; idx++) {
 			var obj = dataSet[idx];
 			searchingDetailLocation = obj.description.toLowerCase();
 
 			var foundAt = searchingDetailLocation.indexOf(detailLocation);
 
-			if(foundAt > -1) {
+			if (foundAt > -1) {
 				var newCardId = `card-${matchNumber}`;
 				var newCard = GetNewTemplate("card_template", newCardId);
 
-				if(newCard) {
+				if (newCard) {
 					SetTemplateElementValue(newCard, "card_id", (matchNumber + 1));
 					SetTemplateElementValue(newCard, "card_camera_id", obj.id);
 					SetTemplateElementValue(newCard, "card_detail_location", obj.description);
@@ -135,20 +135,20 @@ function SearchCommunityName(communityName) {
 	ToggleClassState("output_field", "hidden", false);
 	ClearTemplateCollection("cards");
 
-	if(communityName && communityName.length > 0) {
+	if (communityName && communityName.length > 0) {
 		var searchingCommunityName; 
 		var matchNumber = 0;
 		communityName = communityName.toLowerCase();
 
-		for(var idx = 0; idx < dataSet.length; idx++) {
+		for (var idx = 0; idx < dataSet.length; idx++) {
 			var obj = dataSet[idx];
 			searchingCommunityName = obj.community.toLowerCase();
 
-			if(searchingCommunityName.startsWith(communityName)) {
+			if (searchingCommunityName.startsWith(communityName)) {
 				var newCardId = `card-${matchNumber}`;
 				var newCard = GetNewTemplate("card_template", newCardId);
 
-				if(newCard) {
+				if (newCard) {
 					SetTemplateElementValue(newCard, "card_id", (matchNumber + 1));
 					SetTemplateElementValue(newCard, "card_camera_id", obj.id);
 					SetTemplateElementValue(newCard, "card_detail_location", obj.description);
@@ -169,6 +169,223 @@ function SearchCommunityName(communityName) {
 
 		if (matchNumber == 0) {
 			SetElementInnerHTML("cards", `No matches found for '${communityName}'`);
+		}
+	}
+}
+
+function SearchBuildingPermitNumber(buildingPermitNumber) { 
+	SetElementInnerHTML("search_value", "Search by Permit Number"); 
+	ToggleClassState("output_field", "hidden", false); 
+	ClearTemplateCollection("cards");
+
+	if (buildingPermitNumber && buildingPermitNumber.length > 0) {
+		var searchingBuildingPermitNumber; 
+		var matchNumber = 0;
+        buildingPermitNumber = buildingPermitNumber.toLowerCase();
+
+		for (var idx = 0; idx < dataSet.length; idx++) {
+			var obj = dataSet[idx];
+			searchingBuildingPermitNumber = obj.permitnum.toLowerCase();
+
+			if (searchingBuildingPermitNumber == buildingPermitNumber) {
+				var newCardId = `card-${matchNumber}`;
+				var newCard = GetNewTemplate("card_template", newCardId);
+				var currencyOfCanada = {
+					style: "currency", currency: "CAD"
+				};
+
+				if (newCard) {
+					SetTemplateElementValue(newCard, "card_id", (matchNumber + 1));
+					SetTemplateElementValue(newCard, "card_building_permit_number", obj.permitnum);
+					SetTemplateElementValue(newCard, "card_applied_date", obj.applieddate.slice(0, 10));
+					SetTemplateElementValue(newCard, "card_permit_type", obj.permittype);
+					SetTemplateElementValue(newCard, "card_permit_class", obj.permitclass);
+					SetTemplateElementValue(newCard, "card_permit_group", obj.permitclassgroup);
+					SetTemplateElementValue(newCard, "card_permit_description", obj.description);
+					SetTemplateElementValue(newCard, "card_project_cost",
+						(obj.estprojectcost * 1).toLocaleString("en-CA", currencyOfCanada));
+					SetTemplateElementValue(newCard, "card_building_address", obj.originaladdress);
+					SetTemplateElementValue(newCard, "card_community_name", obj.communityname);
+					SetTemplateElementValue(newCard, "link_map",
+						`<a href="https://www.google.com/maps/place/${obj.latitude},${obj.longitude}" target="_blank">
+						View in Google Maps</a>`);
+
+					AddTemplateToCollection("cards", newCard);
+				} else {
+					console.log(`Could not create new card template`);
+				}
+
+				matchNumber++;
+			}
+		}
+
+		if (matchNumber == 0) {
+			SetElementInnerHTML("cards", `No matches found for '${buildingPermitNumber}'`);
+		}
+	}
+}
+
+function SearchAppliedDate(appliedDate) { 
+	SetElementInnerHTML("search_value", "Search by Applied Date"); 
+	ToggleClassState("output_field", "hidden", false); 
+	ClearTemplateCollection("cards");
+
+	if (appliedDate && appliedDate.length > 0) {
+		var searchingAppliedDate; 
+		var matchNumber = 0;
+
+		for (var idx = 0; idx < dataSet.length; idx++) {
+			var obj = dataSet[idx];
+			searchingAppliedDate = obj.applieddate.slice(0, 10);
+
+			if (searchingAppliedDate == appliedDate) {
+				var newCardId = `card-${matchNumber}`;
+				var newCard = GetNewTemplate("card_template", newCardId);
+				var currencyOfCanada = {
+					style: "currency", currency: "CAD"
+				};
+
+				if (newCard) {
+					SetTemplateElementValue(newCard, "card_id", (matchNumber + 1));
+					SetTemplateElementValue(newCard, "card_building_permit_number", obj.permitnum);
+					SetTemplateElementValue(newCard, "card_applied_date", obj.applieddate.slice(0, 10));
+					SetTemplateElementValue(newCard, "card_permit_type", obj.permittype);
+					SetTemplateElementValue(newCard, "card_permit_class", obj.permitclass);
+					SetTemplateElementValue(newCard, "card_permit_group", obj.permitclassgroup);
+					SetTemplateElementValue(newCard, "card_permit_description", obj.description);
+					SetTemplateElementValue(newCard, "card_project_cost",
+						(obj.estprojectcost * 1).toLocaleString("en-CA", currencyOfCanada));
+					SetTemplateElementValue(newCard, "card_building_address", obj.originaladdress);
+					SetTemplateElementValue(newCard, "card_community_name", obj.communityname);
+					SetTemplateElementValue(newCard, "link_map",
+						`<a href="https://www.google.com/maps/place/${obj.latitude},${obj.longitude}" target="_blank">
+						View in Google Maps</a>`);
+
+					AddTemplateToCollection("cards", newCard);
+				} else {
+					console.log(`Could not create new card template`);
+				}
+
+				matchNumber++;
+			}
+		}
+
+		if (matchNumber == 0) {
+			SetElementInnerHTML("cards", `No matches found for '${appliedDate}'`);
+		}
+	}
+}
+
+function SearchPermitKeyword(permitKeyword) {
+	SetElementInnerHTML("search_value", "Search by Permit Keyword");
+	ToggleClassState("output_field", "hidden", false);
+	ClearTemplateCollection("cards");
+
+	if (permitKeyword && permitKeyword.length > 0) {
+		var searchingPermitType; 
+		var searchingPermitClass;
+		var searchingPermitGroup;
+		var matchNumber = 0;
+		permitKeyword = permitKeyword.toLowerCase();
+
+		for (var idx = 0; idx < dataSet.length; idx++) {
+			var obj = dataSet[idx];
+			searchingPermitType = obj.permittype.toLowerCase();
+			searchingPermitClass = obj.permitclass.toLowerCase();			
+			searchingPermitGroup = obj.permitclassgroup.toLowerCase();
+
+			var foundAtType = searchingPermitType.indexOf(permitKeyword);
+			var foundAtClass = searchingPermitClass.indexOf(permitKeyword);
+			var foundAtGroup = searchingPermitGroup.indexOf(permitKeyword);
+
+			if (foundAtType > -1 || foundAtClass > -1 || foundAtGroup > -1) {
+				var newCardId = `card-${matchNumber}`;
+				var newCard = GetNewTemplate("card_template", newCardId);
+				var currencyOfCanada = {
+					style: "currency", currency: "CAD"
+				};
+
+				if (newCard) {
+					SetTemplateElementValue(newCard, "card_id", (matchNumber + 1));
+					SetTemplateElementValue(newCard, "card_building_permit_number", obj.permitnum);
+					SetTemplateElementValue(newCard, "card_applied_date", obj.applieddate.slice(0, 10));
+					SetTemplateElementValue(newCard, "card_permit_type", obj.permittype);
+					SetTemplateElementValue(newCard, "card_permit_class", obj.permitclass);
+					SetTemplateElementValue(newCard, "card_permit_group", obj.permitclassgroup);
+					SetTemplateElementValue(newCard, "card_permit_description", obj.description);
+					SetTemplateElementValue(newCard, "card_project_cost",
+						(obj.estprojectcost * 1).toLocaleString("en-CA", currencyOfCanada));
+					SetTemplateElementValue(newCard, "card_building_address", obj.originaladdress);
+					SetTemplateElementValue(newCard, "card_community_name", obj.communityname);
+					SetTemplateElementValue(newCard, "link_map",
+						`<a href="https://www.google.com/maps/place/${obj.latitude},${obj.longitude}" target="_blank">
+						View in Google Maps</a>`);
+					
+					AddTemplateToCollection("cards", newCard);
+				} else {
+					console.log(`Could not create new card template`);
+				}
+
+				matchNumber++;
+			}
+		}
+
+		if (matchNumber == 0) {
+			SetElementInnerHTML("cards", `No matches found for '${permitKeyword}'`);
+		}
+	}
+}
+
+function SearchBuildingAddress(buildingAddress) {
+	SetElementInnerHTML("search_value", "Search by Address");
+	ToggleClassState("output_field", "hidden", false);
+	ClearTemplateCollection("cards");
+
+	if (buildingAddress && buildingAddress.length > 0) {
+		var searchingBuildingAddress; 
+		var matchNumber = 0;
+		buildingAddress = buildingAddress.toLowerCase();
+
+		for (var idx = 0; idx < dataSet.length; idx++) {
+			var obj = dataSet[idx];
+			searchingBuildingAddress = obj.originaladdress.toLowerCase();
+
+			var foundAt = searchingBuildingAddress.indexOf(buildingAddress);
+
+			if (foundAt > -1) {
+				var newCardId = `card-${matchNumber}`;
+				var newCard = GetNewTemplate("card_template", newCardId);
+				var currencyOfCanada = {
+					style: "currency", currency: "CAD"
+				};
+
+				if (newCard) {
+					SetTemplateElementValue(newCard, "card_id", (matchNumber + 1));
+					SetTemplateElementValue(newCard, "card_building_permit_number", obj.permitnum);
+					SetTemplateElementValue(newCard, "card_applied_date", obj.applieddate.slice(0, 10));
+					SetTemplateElementValue(newCard, "card_permit_type", obj.permittype);
+					SetTemplateElementValue(newCard, "card_permit_class", obj.permitclass);
+					SetTemplateElementValue(newCard, "card_permit_group", obj.permitclassgroup);
+					SetTemplateElementValue(newCard, "card_permit_description", obj.description);
+					SetTemplateElementValue(newCard, "card_project_cost",
+						(obj.estprojectcost * 1).toLocaleString("en-CA", currencyOfCanada));
+					SetTemplateElementValue(newCard, "card_building_address", obj.originaladdress);
+					SetTemplateElementValue(newCard, "card_community_name", obj.communityname);
+					SetTemplateElementValue(newCard, "link_map",
+						`<a href="https://www.google.com/maps/place/${obj.latitude},${obj.longitude}" target="_blank">
+						View in Google Maps</a>`);
+					
+					AddTemplateToCollection("cards", newCard);
+				} else {
+					console.log(`Could not create new card template`);
+				}
+
+				matchNumber++;
+			}
+		}
+
+		if (matchNumber == 0) {
+			SetElementInnerHTML("cards", `No matches found for '${buildingAddress}'`);
 		}
 	}
 }
